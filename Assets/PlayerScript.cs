30b42b29792ee56e1a8dd880f3ea5aabc4a0123e
Edvard-Erickson <ericksoneg22@gcc.edu>
2024-11-04 10:09:14 -0500

    created player and mouse tracking

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    private Camera mainCam;
+    private Vector3 mousePos;
+
+    public GameObject Projectile;
+    public Transform endOfGun;
+
+    private float timer;
+
+    public bool canFire;
+    public float timeBetweenFiring;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector3 rotation = mousePos - transform.position;
+
+        float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0, 0, rotZ);
+
+        if (!canFire)
+        {
+            timer += Time.deltaTime;
+            if(timer > timeBetweenFiring)
+            {
+                canFire = true;
+                timer = 0;
+            }
+        }
+
+        if (Input.GetMouseButton(0) && canFire)
+        {
+            canFire = false;
+            Instantiate(Projectile, endOfGun.position, Quaternion.identity);
+
+        }
+    }
+}
